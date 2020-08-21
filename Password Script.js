8da3c4c37449653967f3a19201a88b1b4524ba6a
Rivenception <ricky.mingione@gmail.com>
2020-08-21 16:50:28 -0400

    uodates tos tyle sheet and html

--- Password Script.js
+++ Password Script.js
@@ -9,37 +9,41 @@ var mainEl = document.getElementById("main");
 var timerEl = document.getElementById("countdown");
 var bodyEl = document.createElement("div");
 
-// NEED REPEATING FUNCTION HERE
 
-var repeats = prompt("How many characters would you like for your password?")
-var charRepeats = Math.floor(repeats) + 1
+// PROMPT ASKING HOW CHARACTERS THE USER WANTS FOR A PASSWORD
+var repeats = prompt("How many characters would you like for your password?");
+var charRepeats = Math.floor(repeats) + 1;
 
+// THIS FOR LOOP RETURNS A NEW PASSWORD
 
-for (let x = 0; x < 8; x++) {
-    function genKey() {
-        var randLNS = Math.floor(Math.random() * 3);
+for (let x = 0; x < charRepeats; x++) {
+    genKey()
+    var newPassword = password.join("");
+}
 
-        if (randLNS === 0) {
-            var letterRand = Math.floor(Math.random() * 27);
-            var isCapital = Math.floor(Math.random() * 2);
-            var pwgen = singleLetters[letterRand];
-            if (isCapital === 0) {
-                pwgen.toUpperCase().charAt(0);
-            } else {
-                pwgen.toLowerCase();
-            }
-            password.push(pwgen);
-        } else if (randLNS === 1) {
-            var numberRand = Math.floor(Math.random() * 10);
-            var pwgen = numbers[numberRand];
-            password.push(pwgen);
+// CODE FOR RANDOM GENERATION OF EACH CHARACTER
+function genKey() {
+    var randLNS = Math.floor(Math.random() * 3);
+
+    if (randLNS === 0) {
+        var letterRand = Math.floor(Math.random() * 27);
+        var isCapital = Math.floor(Math.random() * 2);
+        var pwgen = singleLetters[letterRand];
+        if (isCapital === 0) {
+            pwgen.toUpperCase().charAt(0);
         } else {
-            var symbolRand = Math.floor(Math.random() * 10);
-            var pwgen = singleSymbols[symbolRand];
-            password.push(pwgen);
+            pwgen.toLowerCase();
         }
+        password.push(pwgen);
+    } else if (randLNS === 1) {
+        var numberRand = Math.floor(Math.random() * 10);
+        var pwgen = numbers[numberRand];
+        password.push(pwgen);
+    } else {
+        var symbolRand = Math.floor(Math.random() * 10);
+        var pwgen = singleSymbols[symbolRand];
+        password.push(pwgen);
     }
-    var newPassword = password.join();
 }
 
 alert("Your new password is " + newPassword)
--- README.md
+++ README.md
@@ -1,49 +1,27 @@
 
-# 01 HTML CSS Git: Password Generator Homework_Three: Uconn Coding Bootcamp
+# 03 Javascript Git: Password Generator Homework_Three: Uconn Coding Bootcamp
 
 
-As a beginner introduction to html refactoring we were tasked with an HTML file and "cleaning up" the code for the need of a client.
-
+As a beginner introduction to javascript we were tasked with building a password generator.
 
 ## The Assignment
 
 ```
-CLIENT: A marketing agency
-REQUEST: Clean the coding that follows accessibility standards for HTML5
-DELIVERABLE: A site is optimized for any search engines
+REQUEST: Create a password generator with an HTML GUI
+DELIVERABLE: A site that can create a new password for the user at the click of a button
 ```
 
 ## Acceptance Criteria
 
 ```
-GIVEN a webpage meets accessibility standards
-WHEN I view the source code
-THEN I find semantic HTML elements
-WHEN I view the structure of the HTML elements
-THEN I find that the elements follow a logical structure independent of styling and positioning
-WHEN I view the image elements
-THEN I find accessible alt attributes
-WHEN I view the heading attributes
-THEN they fall in sequential order
-WHEN I view the title element
-THEN I find a concise, descriptive title
+WHEN I open a webpage I see a prompt to enter how many characters I want for a password.
+THEN A password is returned to me.
 ```
 
 ## Changelog
 
 
-08/05/20 - Rick Mingione
+08/18/20 - Rick Mingione
 
-1. Changed site head active title
-2. Updated all <div> elements to semantic elements for readability and accessability
-3. Added <!--Comments--> to index file for faster editability.
-4. Fixed issues with header links to jump to the required summary location. Effectively created a "Jump-Link"
-5. Image line updated for incorrect syntax
-6. Added alt attributes for images
-
-08/08/20 - Rick Mingione
-
-1. Updated CSS file for condensing classes and adding comments
-2. Added meta="viewport" to html file for application for multiple devices
-3. Reduced images sizes and updated html/css to pull smaller img files for faster load time.
+1. Built the function for creating the password. Pending a loop for # of characters requested by the user for the password.
 
